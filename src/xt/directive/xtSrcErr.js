af7778540a5ea0f6cd1284accb3266839b3eecec
Tim <tim.ruecker@ymail.com>
2015-05-23 17:22:50 +0200

    adding a simple documentation for the xtSrcErr Directive

--- src/xt/directive/xtSrcErr.js
+++ src/xt/directive/xtSrcErr.js
@@ -1,3 +1,14 @@
+/**
+ * @ngdoc directive
+ * @name xt.directive:xtSrcErr
+ * @restrict A
+ * @description
+ * This Directive allowes you to define an alternative source if the source loaded with src or ngSrc is not found.
+ * It works like the alt attribute, just that you load another image not a text.
+ * @scope
+ * @param {string} xtSrcErr Die Alternat Source Image Url
+ */
+
 var xtSrcErrDirective = function () {
     return {
         restrict: 'A',
