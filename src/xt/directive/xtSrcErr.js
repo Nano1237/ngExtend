1391081a6ab0293f2802da247f27e24542aac838
Tim Rücker <tim.ruecker@mcbring.de>
2015-07-23 12:38:05 +0200

    changing the xtSrcErr Directive that it doesnt defines a new scope

--- src/xt/directive/xtSrcErr.js
+++ src/xt/directive/xtSrcErr.js
@@ -5,18 +5,19 @@
  * @description
  * This Directive allows you to define an alternative source if the source loaded with src or ngSrc is not found.
  * It works like the alt attribute, just that you load another image not a text.
- * @scope
- * @param {string} xtSrcErr Die Alternate Source Image Url
+ * @param {string} xtSrcErr Die Alternate Source Image Url or a variable containing the url
  */
 
 function xtSrcErrDirective() {
     return {
         restrict: 'A',
-        scope: {xtSrcErr: '=xtSrcErr'},
         link: function ($scope, $element, $attr) {
+            var regex_isString = /(^['"]|['"]$)/g,
+                isValue = regex_isString.test($attr.xtSrcErr);
             $element.bind('error', function () {
-                if ($attr.src != $scope.xtSrcErr) {
-                    $attr.$set('src', $scope.xtSrcErr);
+                var value = isValue ? $attr.xtSrcErr.replace(regex_isString, '') : $scope[$attr.xtSrcErr];
+                if ($attr.src != value) {
+                    $attr.$set('src', value);
                 }
             });
         }
